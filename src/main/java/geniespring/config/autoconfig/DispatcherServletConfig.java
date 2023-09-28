a51a14a61babd6b8aa546e763d2411b6f84bfcc0
alphajin1 <alphajin1@gmail.com>
2023-09-28 19:16:39 +0900

    ch06 자동 구성 애노테이션 적용

--- src/main/java/geniespring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/geniespring/config/autoconfig/DispatcherServletConfig.java
@@ -1,11 +1,11 @@
 package geniespring.config.autoconfig;
 // package 가 다르므로, ComponentScan 대상이 아님
 
+import geniespring.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration // MetaAnnotation
+@MyAutoConfiguration // MetaAnnotation
 public class DispatcherServletConfig {
 
     @Bean
