72d3f9ddf314250dffeaa695d57474de44883f06
alphajin1 <alphajin1@gmail.com>
2023-10-01 17:46:10 +0900

    ch08 자동 구성에 Env Properties 적용

--- src/main/java/geniespring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/geniespring/config/autoconfig/TomcatWebServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 // @MyAutoConfiguration ->
 // ImportSelector 와 같이 맞춰주자.
@@ -15,10 +16,12 @@ import org.springframework.context.annotation.Bean;
 public class TomcatWebServerConfig {
     @Bean("TomcatWebServerFactory")
     @ConditionalOnMissingBean // 이미 등록된 것이 없을 떄에만 등록할 것 (Type 인듯?)
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
 
         // TomcatServletContainer
-        return new TomcatServletWebServerFactory();
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 
 }
